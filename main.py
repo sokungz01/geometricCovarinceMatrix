53dcaf28f53862870558c0c064b0740389bf34b1
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2023-12-16 18:14:26 +0700

    :pencil: Typo : Renamed label for graph

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ print(df)
 eigenvalues, eigenvectors = np.linalg.eig(cov_df)
 standard = preprocessing.scale(df)
 x_data = df['income']
+x_label = 'Income'
 y_data = df['expense']
+y_label = 'Expense'
 plt.figure(figsize=(16,9))
 
 #FIGURE 1
@@ -23,7 +25,7 @@ xmin, xmax=plt.xlim()
 x=np.linspace(xmin,xmax,100)
 p=norm.pdf(x, mean, std)
 plt.plot(x,p,'k',linewidth=1)
-plt.title('Gaussian Function with x [mean = %.3f and standard deviation =%.3f]'%(mean,std))
+plt.title('Gaussian Function with %s [mean = %.3f and standard deviation =%.3f]'%(x_label,mean,std))
 plt.grid(True)
 
 #FIGURE 2
@@ -34,7 +36,7 @@ xmin, xmax=plt.xlim()
 x=np.linspace(xmin,xmax,100)
 p=norm.pdf(x, mean, std)
 plt.plot(x,p,'k',linewidth=1)
-plt.title('Gaussian Function with yuency [mean = %.3f and standard deviation =%.3f]'%(mean,std))
+plt.title('Gaussian Function with %s [mean = %.3f and standard deviation =%.3f]'%(y_label,mean,std))
 plt.grid(True)
 
 #FIGURE 3
@@ -42,8 +44,8 @@ plt.subplot(2,3,4)
 plt.scatter(x_data, y_data)
 plt.title(f'{dataName} Raw Data')
 plt.grid(True)
-plt.xlabel("x")
-plt.ylabel("y")
+plt.xlabel(f'{x_label}')
+plt.ylabel(f'{y_label}')
 
 #FIGURE 4
 plt.subplot(2,3,5)
@@ -53,6 +55,8 @@ col= [f'Sample_{i}' for i in range (sample.shape[1])]
 gen_df =pd.DataFrame(sample,columns=col)
 plt.scatter(sample[:,0],sample[:,1])
 plt.title(f"{dataName} Co-Varince")
+plt.xlabel(f'{x_label}')
+plt.ylabel(f'{y_label}')
 plt.grid(True)
 
 #FIGURE 5
@@ -61,8 +65,8 @@ plt.scatter(standard[:, 1], standard[:, 0], alpha=0.5)
 plt.title(f"{dataName} Standardized Data with eigenvectors")
 plt.arrow(0,0,eigenvectors[0,0] ,eigenvectors[0,1],head_width=0.1,head_length=0.2,color='r')
 plt.arrow(0,0,eigenvectors[1,0]*3 ,eigenvectors[1,1]*3,head_width=0.1,head_length=0.2,color='g')
-plt.xlabel('Standardize x')
-plt.ylabel('Standardize y')
+plt.xlabel(f'Standardize {x_label}')
+plt.ylabel(f'Standardize {y_label}')
 plt.grid(True)
 
 
